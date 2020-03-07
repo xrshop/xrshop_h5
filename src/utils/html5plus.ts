30885efff1e1c75c45d52a499d65b590a6cc651c
lzq <1468743505@qq.com>
2020-03-07 17:02:38 +0800

    修复 plus bug

--- src/utils/html5plus.ts
+++ src/utils/html5plus.ts
@@ -1,7 +1,13 @@
+declare global {
+  interface Window {
+    plus: Plus;
+  }
+}
+
 // 根据 plus 初始化情况进行调用或等待 plus 初始化完毕调用
 export default function plusready(handler: () => void) {
   // 添加的函数一定会执行
-  if (plus) {
+  if (window.plus) {
     handler();
   } else {
     document.addEventListener('plusready', handler, { once: true });
