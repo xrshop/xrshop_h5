9a8497de2f2f0ab440ea26dced318a7e77e015c3
付佳欢 <1208761178@qq.com>
2020-04-11 18:26:18 +0800

    积分明细,部分积分商品

---
+++ src/views/IntegralDetails.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="integral-details">
+    <TitleBar title="积分明细" canBack />
+    <div class="block">
+      <div class="text">6879<span>积分</span></div>
+    </div>
+    <div class="list-box">
+      <div class="item-box">
+        <div class="item title">
+          <div class="left">
+            <div class="text">积分</div>
+          </div>
+          <div class="right">
+            <div class="gain">获取 <span class="red">188</span></div>
+            <div class="use">使用 44</div>
+          </div>
+        </div>
+        <div v-for="index in 8" class="item" :key="index">
+            <div class="left">
+              <div class="text">确认收货</div>
+              <div class="time">2020-03-12 12:30:56</div>
+            </div>
+            <div class="right red">+2</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+:root {
+  font-family:PingFang SC;
+}
+.block {
+  height: 57.6vw;
+  background: rgba(255,240,240,1);
+  border-radius: 0vw 0vw 2vw 2vw;
+  padding-top: 13.47vw;
+  padding-left: 5.2vw;
+  box-sizing: border-box;
+  .text {
+    font-size: 6.67vw;
+    font-weight: bold;
+    span {
+      font-size: 3.2vw;
+      font-weight: 400;
+    }
+  }
+}
+.list-box {
+  position: relative;
+  top: -33.2vw;
+  width: 89.6vw;
+  margin: 0 auto -20vw;
+  .item-box {
+    z-index: 1;
+    position: relative;
+    box-sizing: border-box;
+    padding: 0 4vw 4vw;
+    background: #fff;
+    border-radius: 2vw;
+    .item {
+      &.title {
+        font-weight: bold;
+        .right .gain{
+          margin-right: 4vw;
+        }
+      }
+      display: flex;
+      padding: 5.2vw 0;
+      border-bottom: solid var(--px) #eee;
+      justify-content: space-between;
+      .left {
+        .text {
+          font-size: 4vw;
+        }
+        .time {
+          font-size: 2.67vw;
+          color: rgba(187,187,187,1);
+          margin-top: 2.67vw;
+        }
+      }
+      .right {
+        display: flex;
+      }
+    }
+    .red {
+      color: #F84E4E;
+    }
+  }
+  &::after {
+    content: '';
+    display: block;
+    width: 100%;
+    height: calc(100% - 17vw);
+    position: absolute;
+    bottom: 0;
+    z-index: 0;
+    border-radius: 2vw;
+    box-shadow: 0 0 3vw 0.8vw rgba(175,180,184,0.2);
+  }
+}
+</style>
---
+++ src/views/IntegralGoods.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="integral-goods">
+    <TitleBar rightText=" " @rightTextClick="shar" canBack />
+    <div class="banner" v-for="(item,index) in img.banner" :key="index">
+      <div class="cover" :style="{'background-image': `url(${item})`}"></div>
+    </div>
+    <div class="footer">
+      <div class="but">立即兑换</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      img: {
+        banner: ['https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1539655970,3080509067&fm=26&gp=0.jpg', 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1539655970,3080509067&fm=26&gp=0.jpg'],
+        publicity: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586606284942&di=be237b85c55069eba52036e732305f8c&imgtype=0&src=http%3A%2F%2Fwww.zbzdjbx.cn%2Fuploads%2Ftk12032274.jpg', 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586606300832&di=1943ebca864f1a6a19348e9d7209e5b3&imgtype=0&src=http%3A%2F%2Fcp2.douguo.net%2Fupload%2Fdish%2Fd%2F4%2F3%2F600_d4e9a1c2bd0fa0cc8378680cd26cccf3.jpg'],
+      },
+    };
+  },
+  methods: {
+    shar() {
+      return console.log(1);
+    },
+  },
+};
+
+</script>
+
+<style lang="scss" scoped>
+.title-bar::v-deep {
+  .main {
+    .right,.left {
+      width: 6.67vw;
+      height: 6.67vw;
+      background: #000;
+      border-radius: 50%;
+      top: 50%;
+      transform: translateY(-50%);
+      opacity: .5;
+    }
+    .right {
+      right: 5.47vw;
+      padding: none;
+    }
+    .left {
+      left: 5.07vw;
+      .back-button {
+        width: 100%;
+        height: 100%;
+        >svg{
+          width: 80%;
+          height: 80%;
+          path {
+          fill: rgb(255, 255, 255)
+        }
+        }
+      }
+    }
+  }
+}
+.footer {
+  height: 17.07vw;
+  width: 100vw;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: PingFang SC;
+  box-shadow: 0 0 6px 0px rgba(200,200,200,0.3);
+  .but {
+    width: 89.33vw;
+    text-align: center;
+    line-height: 11.73vw;
+    background: rgba(248,78,78,1);
+    border-radius: 5.87vw;
+    font-size: 4vw;
+    color: #fff;
+  }
+}
+</style>
