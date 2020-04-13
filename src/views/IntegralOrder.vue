7a868faa86fd6534b569b313181c8c5c3f97d3e3
付佳欢 <1208761178@qq.com>
2020-04-13 18:31:00 +0800

    完善积分模块

---
+++ src/views/IntegralOrder.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="integral-order">
+    <TitleBar title="确认订单" canBack />
+    <div class="site">
+      <img src="@/assets/IntegralOrder/dz.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+.integral-order {
+  background-color: #F7F5F6;
+}
+</style>
