377317aa195eed75e8d20256c594efba26d878fb
付佳欢 <1208761178@qq.com>
2020-05-22 09:53:52 +0800

    创建文件

---
+++ src/views/Index/HomeB/Recommend.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="recomment">
+    <div class="item">
+      <div class="title">
+        <div class="text">精选好物</div>
+        <div class="subtitle"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Recommend',
+};
+</script>
+
+<style>
+
+</style>
