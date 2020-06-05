0c29b49a69dffd201a2dcd01564b38fc5c023bc9
付佳欢 <1208761178@qq.com>
2020-06-05 09:22:14 +0800

    分类详情流布局

--- src/views/CategoryDetails.vue
+++ src/views/CategoryDetails.vue
@@ -5,16 +5,45 @@
         <SearchSimple />
       </template>
       <template v-slot:right>
-        <div class="menu-button"
-        :class="{'style2': changeStyle}"
-        @click="changeStyle = !changeStyle">
-        </div>
+        <div
+          class="menu-button"
+          :class="{'style2': changeStyle}"
+          @click="changeStyle = !changeStyle"
+        ></div>
       </template>
       <template v-slot:other>
         <Tabs :options="sort" v-model="sortActivated" />
       </template>
     </TitleBar>
-    <div class="list" :class="{'row-style': changeStyle}">
+    <waterfall :data="data" :col="2" :isTransition="false" v-show="changeStyle == false">
+      <template>
+        <router-link to="/item-details" class="cell" v-for="item of data" :key="item.id">
+          <div
+            class="cover"
+            :style="{'background-image':'url(https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3507028413,2530873393&fm=26&gp=0.jpg)'}"
+          ></div>
+          <div class="info">
+            <div class="title">{{item.title}}</div>
+            <div class="label">
+              <div class="item">冷冻</div>
+            </div>
+            <div class="row-a">
+              <div class="money">
+                <Price :value="56.80" />
+                <div class="type">
+                  <div class="item stress">自营</div>
+                  <div class="item">满减</div>
+                  <div class="item">惠</div>
+                </div>
+              </div>
+              <span class="count">236人买过</span>
+            </div>
+          </div>
+          <img src="@/assets/CategoryDetails/gwc.png" class="icon" alt />
+        </router-link>
+      </template>
+    </waterfall>
+    <div class="changeStyle" v-show="changeStyle">
       <router-link to="/item-details" class="cell" v-for="index of 20" :key="index">
         <div
           class="cover"
@@ -26,13 +55,15 @@
             <div class="item">冷冻</div>
           </div>
           <div class="row-a">
-            <Price :value="56.80" />
-            <span class="count">236人买过</span>
-            <div class="type">
-              <div class="item stress">自营</div>
-              <div class="item">满减</div>
-              <div class="item">惠</div>
+            <div class="money">
+              <Price :value="56.80" />
+              <div class="type">
+                <div class="item stress">自营</div>
+                <div class="item">满减</div>
+                <div class="item">惠</div>
+              </div>
             </div>
+            <span class="count">236人买过</span>
           </div>
         </div>
         <img src="@/assets/CategoryDetails/gwc.png" class="icon" alt="">
@@ -53,6 +84,12 @@ export default {
       ],
       sortActivated: 1,
       changeStyle: false,
+      data: [
+        { id: 0, title: '西红柿樱桃小番茄水果 荷兰瓜柿子蔬菜', money: 56.8 },
+        { id: 1, title: '柿樱桃小番茄水果 荷兰瓜柿子蔬菜', money: 56.8 },
+        { id: 2, title: '西红柿樱桃小番茄水果 荷兰瓜柿子蔬菜', money: 56.8 },
+        { id: 3, title: '柿樱桃小番茄水果 荷兰瓜柿子蔬菜', money: 56.8 },
+      ],
     };
   },
 };
@@ -87,71 +124,101 @@ export default {
   background-image: url("~@/assets/CategoryDetails/menu-button.png");
 }
 
-.list {
-  display: flex;
-  flex-wrap: wrap;
-  padding: 2.67vw 4vw;
-  justify-content: space-between;
-  .cell {
-    width: 44.67vw;
-    background-color: #fff;
-    border-radius: 2vw;
-    overflow: hidden;
-    box-shadow: 0 0 1vw 0 rgba($color: #000000, $alpha: 0.01);
-    margin-bottom: 2.67vw;
-    .icon {
-      display: none;
+.vue-waterfall::v-deep {
+  padding: 0 3.33vw;
+  box-sizing: border-box;
+  margin-top: 2.67vw;
+  .vue-waterfall-column {
+    padding: 0 0.67vw;
+    box-sizing: border-box;
+    .cell {
+      display: block;
+      width: 44.67vw;
+      background-color: #fff;
+      border-radius: 2vw;
+      overflow: hidden;
+      box-shadow: 0 0 1vw 0 rgba($color: #000000, $alpha: 0.01);
+      margin:0 auto 2.67vw;
+      position: relative;
+      .icon {
+        display: none;
+      }
     }
-  }
-  .cover {
-    height: 44.67vw;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-  .info {
-    padding: 2.67vw;
-  }
-  .title {
-    margin-top: 1.87vw - 2.53vw - (4.53vw - 3.73vw) / 2;
-    font-size: 3.73vw;
-    line-height: 4.53vw;
-  }
-  .mass {
-    margin-top: 2.53vw - (5.2vw - 4vw) / 2;
-    color: #bbb;
-    font-size: 3.2vw;
-  }
-  .row-a {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 3.07vw;
-    .type {
-      display: none;
+    .cover {
+      height: 44.67vw;
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
     }
-  }
-  // .price ::v-deep {
-  //   .float {
-  //     font-size: 3.2vw;
-  //     font-weight: bold;
-  //   }
-  // }
-  .count {
-    font-size: 3.2vw;
-    color: #bbb;
-    // margin-left: 2.67vw;
+    .info {
+      padding: 2.67vw 2.67vw 4vw;
+      .label {
+        margin-top: 2.13vw;
+        .item {
+          width: max-content;
+          padding: 0.53vw 1.33vw;
+          font-size: 2.67vw;
+          color: #999;
+          background-color: #f6f6f6;
+          border-radius: 1.87vw;
+        }
+      }
+    }
+    .title {
+      margin-top: 1.87vw - 2.53vw - (4.53vw - 3.73vw) / 2;
+      font-size: 3.73vw;
+      line-height: 4.53vw;
+    }
+    .mass {
+      margin-top: 2.53vw - (5.2vw - 4vw) / 2;
+      color: #bbb;
+      font-size: 3.2vw;
+    }
+    .row-a {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 3.07vw;
+      .type {
+        display: flex;
+        margin-top: 2.53vw;
+        .item {
+          margin-right: 1.33vw;
+          font-size: 2.67vw;
+          padding: 0.53vw 1.2vw;
+          border: solid var(--px) #ff450b;
+          color: #ff450b;
+          border-radius: 0.4vw;
+          &:last-of-type {
+            margin-right: 0;
+          }
+        }
+        .stress {
+          color: #fff;
+          background-color: #ff450b;
+        }
+      }
+    }
+    .count {
+      position: absolute;
+      font-size: 2.67vw;
+      color: #bbb;
+      right: 4vw;
+    }
   }
 }
-.list.row-style {
+
+.changeStyle {
+  padding: 0 4vw;
   .cell {
     width: 92vw;
     height: 40.13vw;
-    padding: 4vw;
     display: flex;
-    box-sizing: border-box;
     border-radius: 1.33vw;
     position: relative;
+    background-color: #fff;
+    margin-top: 2.67vw;
+    padding: 4vw;
+    box-sizing: border-box;
     .icon {
       display: block;
       width: 7.47vw;
@@ -160,9 +227,14 @@ export default {
       bottom: 9.73vw;
     }
     .cover {
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
       width: 32vw;
       height: 32vw;
       flex-shrink: 0;
+      border-radius: 1.33vw;
+      margin-right: 2.67vw;
     }
     .info {
       position: relative;
@@ -173,7 +245,7 @@ export default {
           padding: 0.53vw 1.33vw;
           font-size: 2.67vw;
           color: #999;
-          background-color: #F6F6F6;
+          background-color: #f6f6f6;
           border-radius: 1.87vw;
         }
       }
@@ -193,13 +265,13 @@ export default {
             margin-right: 1.33vw;
             font-size: 2.67vw;
             padding: 0.53vw 1.2vw;
-            border: solid var(--px) #FF450B;
-            color: #FF450B;
+            border: solid var(--px) #ff450b;
+            color: #ff450b;
+            border-radius: 0.4vw;
           }
           .stress {
             color: #fff;
-            background-color: #FF450B;
-            border-radius: 0.4vw;
+            background-color: #ff450b;
           }
         }
       }
@@ -208,7 +280,7 @@ export default {
 }
 .tabs::v-deep {
   .active {
-    color: #F84E4E;
+    color: #f84e4e;
   }
 }
 </style>
