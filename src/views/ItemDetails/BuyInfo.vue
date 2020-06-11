80568605fa1976adb397b336103906f234ab0121
付佳欢 <1208761178@qq.com>
2020-06-11 18:22:39 +0800

    规格选择, 加入购物车

---
+++ src/views/ItemDetails/BuyInfo.vue
@@ -0,0 +1,210 @@
+<template>
+  <div class="block">
+    <div class="info">
+      <div
+        class="cover"
+        :style="{ 'background-image': `url(${dataLast.image})` }"
+      ></div>
+      <div class="right">
+        <price :value="dataLast.price" />
+        <div class="stock">库存件{{ dataLast.stock }}</div>
+        <div class="text">请选择规格属性</div>
+      </div>
+    </div>
+    <div class="type">
+      <div class="item" v-for="(item, index) in info.productAttr" :key="index">
+        <div class="title">{{ item.attrName }}</div>
+        <div class="list">
+          <div
+            class="cell"
+            v-for="(cell, index) in item.attrValue"
+            :key="index"
+            :class="{ active: options.active == index }"
+            @click="options.active = index"
+          >
+            {{ cell.attr }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom">
+      <div class="left">购买数量</div>
+      <div class="right">
+        <div class="subtract" @click="subtract">-</div>
+        <input type="number" v-model="options.count" readonly />
+        <div class="plus" @click="plus">+</div>
+      </div>
+    </div>
+    <div class="button" @click="confirm()">确定</div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable @typescript-eslint/camelcase */
+
+import axios from 'axios';
+import userManage from '@/modules/user-manage';
+
+export default {
+  name: 'BuyInfo',
+  props: { info: {}, options: {}, clickType: {} },
+  methods: {
+    subtract() {
+      const { count } = this.options;
+      if (count > 1) {
+        this.options.count -= 1;
+      }
+    },
+    plus() {
+      const { count } = this.options;
+      if (count < this.dataLast.stock) {
+        this.options.count += 1;
+      }
+    },
+    confirm() {
+      const { dataLast } = this;
+      if (this.clickType === 1) {
+        axios
+          .post('/api/cart/add', {
+            productId: dataLast.productId,
+            cartNum: this.options.count,
+            isNew: 0,
+            uniqueId: dataLast.unique,
+          },
+          {
+            headers: { Authorization: this.token },
+          })
+          .then((response) => {
+            console.log(response);
+            alert(response.data.msg);
+          });
+      }
+    },
+  },
+  computed: {
+    dataLast() {
+      return this.info.productValue[this.attr];
+    },
+    attr() {
+      return this.info.productAttr[0].attrValueArr[this.options.active];
+    },
+    token() {
+      return userManage.data.token;
+    },
+  },
+  mounted() {
+    console.log(this.info);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.block {
+  background-color: #fff;
+  border-radius: 4vw 4vw 0 0;
+  padding: 4.13vw 4vw 1.33vw;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  box-sizing: border-box;
+  z-index: 100;
+  .info {
+    display: flex;
+    .cover {
+      width: 20vw;
+      height: 20vw;
+      margin-right: 2.67vw;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center center;
+    }
+    .right {
+      margin-top: 1.2vw;
+      .stock {
+        margin-top: 2.67vw;
+        color: #bbb;
+        font-size: 2.67vw;
+      }
+      .text {
+        margin-top: 2.67vw;
+        font-size: 3.2vw;
+      }
+    }
+  }
+  .type {
+    margin-top: 4vw;
+    padding: 4vw 0;
+    border-bottom: solid var(--px) #f9f9f9;
+    border-top: solid var(--px) #f9f9f9;
+    .item {
+      margin-top: 4vw;
+      &:first-of-type {
+        margin-top: 0;
+      }
+    }
+    .title {
+      font-size: 3.2vw;
+    }
+    .list {
+      margin-top: 2.67vw;
+      display: flex;
+      flex-wrap: wrap;
+      .cell {
+        padding: 1.2vw 1.33vw;
+        background-color: #f9f9f9;
+        color: #bbb;
+        font-size: 2.67vw;
+        border-radius: 0.4vw;
+        margin-right: 2vw;
+      }
+      .active {
+        color: #ffffff;
+        background-color: #f84e4e;
+      }
+    }
+  }
+  .bottom {
+    margin-top: 4vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .left {
+      font-size: 3.2vw;
+    }
+    .right {
+      display: flex;
+      align-items: center;
+      .subtract {
+        color: #eeeeee;
+        padding: 2vw;
+        font-size: 3.2vw;
+      }
+      .plus {
+        padding: 2vw;
+        font-size: 3.2vw;
+      }
+      input {
+        width: 10.67vw;
+        height: 5.33vw;
+        outline: none;
+        border: solid var(--px) #eeeeee;
+        text-align: center;
+        font-size: 3.2vw;
+        margin: 0 1.33vw;
+      }
+    }
+  }
+  .button {
+    width: 92vw;
+    height: 11.73vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 20vw auto 0;
+    background-color: #f84e4e;
+    border-radius: 5.87vw;
+    color: #fff;
+    font-size: 4vw;
+  }
+}
+</style>
---
+++ src/views/ItemDetails/Footer.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="footer">
+    <div class="nav-buttons">
+      <router-link class="nav-button" to="/">
+        <img class="icon" src="@/assets/ItemDetails/home.png" alt />
+        <div class="text">首页</div>
+      </router-link>
+      <router-link class="nav-button" to="/index/cart">
+        <img class="icon" src="@/assets/ItemDetails/cart.png" alt />
+        <div class="text">购物车</div>
+      </router-link>
+      <div class="nav-button" @click="collect">
+        <img class="icon" src="@/assets/ItemDetails/like.png" alt />
+        <div class="text">收藏</div>
+      </div>
+    </div>
+    <div class="add-to-cart" @click="addCart">加入购物车</div>
+    <router-link to="/order-confirm" class="buy">立即购买</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  methods: {
+    addCart() {
+      this.$emit('eventCart');
+    },
+    collect() {
+      this.$emit('eventCollect');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  height: 13.07vw;
+  background-color: #fff;
+  position: sticky;
+  bottom: 0;
+  display: flex;
+  .nav-buttons {
+    display: flex;
+    flex-grow: 1;
+  }
+  .nav-button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-grow: 1;
+    .icon {
+      // width: 5vw;
+      height: 7vw;
+      margin-top: 1.2vw;
+      display: block;
+    }
+    .text {
+      font-size: 2.67vw;
+      margin-top: 0.6vw;
+    }
+  }
+  .add-to-cart,
+  .buy {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    width: 28.27vw;
+  }
+  .add-to-cart {
+    background-color: #ffd40c;
+  }
+  .buy {
+    background-color: #f84e4e;
+  }
+}
+</style>
