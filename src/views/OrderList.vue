965db69662643ad0b121593d3cd02340f6838dfe
付佳欢 <1208761178@qq.com>
2020-06-17 18:37:48 +0800

    订单列表

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -2,62 +2,70 @@
   <div class="order-list">
     <TitleBar title="我的订单" canBack>
       <template v-slot:other>
-        <Tabs :options="sort" v-model="sortActivated" @direction-change="sorts" />
+        <Tabs :options="sort" v-model="sortActivated" />
       </template>
     </TitleBar>
     <div class="tab-content">
-      <div class="item">
+      <div class="item" v-for="item in orderList" :key="item.cartId">
         <div class="top">
-          <img src="@/assets/OrderList/shop.png" alt="">
+          <img src="@/assets/OrderList/shop.png" alt="" />
           <h3>耒小阳商城</h3>
-          <div class="state">已发货</div>
+          <div class="state">{{ status[item._status._type] }}</div>
         </div>
-        <router-link to="/order-details" class="main">
-          <img
-          src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585650459489&di=fe8e9b52eb9bafff24c23f03dd27ec25&imgtype=0&src=http%3A%2F%2Ff1.meishipu.com%2Fupload%2F11%2Fnd8upkfxkxx9l0khfrsali5tl0pwns51179074_1.jpg"
-          alt=""
-          />
-          <div class="middle">
-            <div class="intro">纯手工糯米糍糍粑手工年糕湖南 地道特产</div>
-            <div class="label">白糯米糍粑</div>
+        <template v-if="item.cartInfo.length === 1">
+          <router-link to="/order-details" class="main">
+            <img :src="item.cartInfo[0].productInfo.attrInfo.image" alt="" />
+            <div class="middle">
+              <div class="intro">
+                {{ item.cartInfo[0].productInfo.storeName }}
+              </div>
+              <div class="label">
+                {{ item.cartInfo[0].productInfo.attrInfo.suk }}
+              </div>
+            </div>
+            <div class="right">
+              <div class="money">
+                <Price
+                  :value="Number(item.cartInfo[0].productInfo.attrInfo.price)"
+                />
+              </div>
+              <div class="quantity">x{{ item.cartInfo[0].cartNum }}</div>
+            </div>
+          </router-link>
+          <div class="stat">
+            <div class="count">
+              共{{ item.cartInfo[0].cartNum }}件商品 合计：
+            </div>
+            <div class="money"><Price :value="Number(item.payPrice)" /></div>
           </div>
-          <div class="right">
-            <div class="money">￥<span>56</span>.80</div>
-            <div class="quantity">x5</div>
-          </div>
-        </router-link>
-        <div class="stat">
-          <div class="count">共5件商品  合计：</div>
-          <div class="money">￥<span>250</span>.80</div>
-        </div>
+        </template>
+        <template v-else>
+          <router-link to="/order-details" class="main img-box">
+            <img
+              v-for="img in item.cartInfo.slice(0, 3)"
+              :key="img.id"
+              :src="img.productInfo.attrInfo.image"
+              alt=""
+            />
+            <div class="stat">
+              <div class="money"><Price :value="Number(item.payPrice)" /></div>
+              <div class="count">
+                共{{
+                  item.cartInfo.map(item => item.cartNum) | sumCount
+                }}件
+              </div>
+            </div>
+          </router-link>
+        </template>
         <div class="button">
-          <div class="style-one cell">查看物流</div>
-          <div class="style-tow cell">确认收货</div>
-        </div>
-      </div>
-      <div class="item">
-        <div class="top">
-          <img src="@/assets/OrderList/shop.png" alt="">
-          <h3>耒小阳商城</h3>
-          <div class="state">付款</div>
-        </div>
-         <router-link to="/order-details" class="main">
-          <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585650459489&di=fe8e9b52eb9bafff24c23f03dd27ec25&imgtype=0&src=http%3A%2F%2Ff1.meishipu.com%2Fupload%2F11%2Fnd8upkfxkxx9l0khfrsali5tl0pwns51179074_1.jpg" alt="">
-          <div class="middle">
-            <div class="intro">纯手工糯米糍糍粑手工年糕湖南 地道特产</div>
-            <div class="label">白糯米糍粑</div>
-          </div>
-          <div class="right">
-            <div class="money">￥<span>56</span>.80</div>
-            <div class="quantity">x5</div>
-          </div>
-        </router-link>
-        <div class="stat">
-          <div class="count">共5件商品  合计：</div>
-          <div class="money">￥<span>250</span>.80</div>
-        </div>
-        <div class="button">
-          <router-link to="/order-details" class="cell style-tow">去付款</router-link>
+          <!-- <div class="style-one cell">查看物流</div>
+          <div class="style-tow cell">确认收货</div> -->
+          <router-link
+            v-if="sortActivated === 0"
+            to="/order-details"
+            class="cell style-tow"
+            >去付款</router-link
+          >
         </div>
       </div>
     </div>
@@ -65,40 +73,71 @@
 </template>
 
 <script>
+import axios from 'axios';
+import userManage from '@/modules/user-manage';
+
 export default {
   data() {
     return {
       sort: [
-        { id: 0, text: '全部' },
-        { id: 1, text: '待付款' },
-        { id: 2, text: '待发货' },
-        { id: 3, text: '待收货' },
-        { id: 4, text: '待评价' },
+        { id: 100, text: '全部' },
+        { id: 0, text: '待付款' },
+        { id: 1, text: '待发货' },
+        { id: 2, text: '待收货' },
+        { id: 3, text: '待评价' },
       ],
-      sortActivated: 0,
+      sortActivated: Number(this.$route.query.type),
+      orderList: [],
+      status: {
+        0: '待支付',
+        1: '待发货',
+        2: '待收货',
+        3: '待评价',
+      },
     };
+  },
+  computed: {
+    token() {
+      return userManage.data.token;
+    },
+  },
+  filters: {
+    sumCount(v) {
+      let count = 0;
+      v.forEach((item) => {
+        count += item;
+      });
+      return count;
+    },
   },
   watch: {
     sortActivated(v) {
-      // eslint-disable-next-line no-console
-      console.log(v);
+      this.getData(v);
     },
   },
   methods: {
-    sorts(item, index) {
-      // eslint-disable-next-line no-console
-      console.log(item, index);
+    getData(type) {
+      this.orderList = [];
+      axios
+        .get(`/api/order/list?type=${type}`, { headers: { Authorization: this.token } })
+        .then((response) => {
+          this.orderList = response.data.data;
+          console.log(this.orderList);
+        });
     },
+  },
+  created() {
+    this.getData(this.sortActivated);
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .order-list {
-  background-color: #F7F4F8;
+  background-color: #f7f4f8;
 }
 .title-bar::v-deep {
-  background-color: #FF4F59;
+  background-color: #ff4f59;
   border-radius: 0 0 2vw 2vw;
   .center {
     color: #fff;
@@ -115,17 +154,22 @@ export default {
     }
   }
 }
-.tab-content .item{
+.tab-content {
+  margin-bottom: 4vw;
+}
+.tab-content .item {
   display: block;
   width: 100vw;
-  height: 58.67vw;
   margin-top: 2.67vw;
-  background: rgba(255,255,255,1);
+  background: rgba(255, 255, 255, 1);
   border-radius: 2vw;
   overflow: hidden;
   box-sizing: border-box;
   padding: 0 5.2vw;
   position: relative;
+  .price::v-deep {
+    color: #333;
+  }
   .top {
     display: flex;
     margin-top: 4.53vw;
@@ -137,9 +181,9 @@ export default {
     }
     h3 {
       margin: 0;
-      font-size:3.73vw;
-      font-weight:bold;
-      color:rgba(51,51,51,1);
+      font-size: 3.73vw;
+      font-weight: bold;
+      color: rgba(51, 51, 51, 1);
       margin-left: 1vw;
     }
     .state {
@@ -147,13 +191,35 @@ export default {
       right: 0;
       font-size: 3.2vw;
       font-weight: 500;
-      color: rgba(248,78,78,1);
+      color: rgba(248, 78, 78, 1);
     }
   }
   .main {
     margin-top: 4.67vw;
     display: flex;
-    justify-content: space-between;
+    &.img-box {
+      position: relative;
+      .stat {
+        height: 20vw;
+        padding-left: 6vw;
+        background-color: #fff;
+        position: absolute;
+        right: 0;
+        top: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        justify-content: center;
+        background-image: url("~@/assets/OrderList/yy.png");
+        background-repeat: no-repeat;
+        background-position: left center;
+        margin: 0;
+        .count {
+          color: #bbb;
+          margin-top: 1.37vw;
+        }
+      }
+    }
     img {
       width: 20vw;
       height: 20vw;
@@ -162,29 +228,30 @@ export default {
       flex-shrink: 0;
     }
     .middle {
+      width: 100%;
       .intro {
-        font-size:3.2vw;
-        font-weight:500;
-        color:rgba(51,51,51,1);
-        line-height:5.2vw;
+        font-size: 3.2vw;
+        font-weight: 500;
+        color: rgba(51, 51, 51, 1);
+        line-height: 5.2vw;
         margin-top: 1vw;
       }
       .label {
         display: inline-block;
         font-size: 2.67vw;
-        color: #BBBBBB;
+        color: #bbbbbb;
         margin-top: 0.8vw;
       }
     }
     .right {
       background-color: #fff;
       margin-top: 2vw;
-      font-weight:bold;
-      color:rgba(51,51,51,1);
+      font-weight: bold;
+      color: rgba(51, 51, 51, 1);
       text-align: right;
       margin-left: 2vw;
       .money {
-        font-size:2.4vw;
+        font-size: 2.4vw;
         span {
           width: 15vw;
           font-size: 3.73vw;
@@ -194,50 +261,49 @@ export default {
         margin-top: 0.8vw;
         text-align: right;
         font-size: 2.67vw;
-        color: #BBBBBB;
+        color: #bbbbbb;
       }
     }
   }
   .stat {
-    position: absolute;
     display: flex;
-    bottom: 17.47vw;
-    right: 5.2vw;
+    margin-top: 4vw;
+    justify-content: flex-end;
     .count {
-      font-size:3.2vw;
-      font-weight:500;
-      color:rgba(51,51,51,1);
+      font-size: 3.2vw;
+      font-weight: 500;
+      color: rgba(51, 51, 51, 1);
       transform: translateY(0.4vw);
     }
     .money {
-      font-size:2.4vw;
-      font-weight:bold;
-      color:rgba(51,51,51,1);
+      font-size: 2.4vw;
+      font-weight: bold;
+      color: rgba(51, 51, 51, 1);
       span {
         font-size: 3.73vw;
       }
     }
   }
   .button {
-    position: absolute;
-    right: 5.2vw;
-    bottom: 5.47vw;
+    padding-bottom: 5.47vw;
     display: flex;
+    justify-content: flex-end;
+    margin-top: 4vw;
     .cell {
       margin-left: 3.2vw;
-      width:24vw;
-      line-height:8vw;
+      width: 24vw;
+      line-height: 8vw;
       text-align: center;
-      border-radius:4vw;
-      font-size:3.2vw;
+      border-radius: 4vw;
+      font-size: 3.2vw;
     }
     .style-one {
-      border: var(--px) solid #BBBBBB;
+      border: var(--px) solid #bbbbbb;
       font-size: 3.2vw;
     }
     .style-tow {
-      border: var(--px) solid #F84E4E;
-      color: #F84E4E;
+      border: var(--px) solid #f84e4e;
+      color: #f84e4e;
     }
   }
 }
