1729e7bb403554246c8d83697d6884a63071ad32
付佳欢 <1208761178@qq.com>
2020-07-07 14:52:55 +0800

    debug

--- src/views/OrderConfirm.vue
+++ src/views/OrderConfirm.vue
@@ -197,12 +197,25 @@ export default {
           this.$router.replace({ path: '/order-details', query: { key: data.result.key } });
         } else if (data.status === 'WECHAT_PAY') {
           console.log(data.result.jsConfig);
-          wechat.chooseWXPay({
-            ...data.result.jsConfig,
-            success(res) {
-              console.log(res);
+          // eslint-disable-next-line no-undef
+          WeixinJSBridge.invoke(
+            'getBrandWCPayRequest', {
+              ...data.result.jsConfig,
             },
-          });
+            (res) => {
+              alert(res);
+              if (res.err_msg === 'get_brand_wcpay_request:ok') {
+                // 使用以上方式判断前端返回,微信团队郑重提示：
+                // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+              }
+            },
+          );
+          // wechat.chooseWXPay({
+          //   ...data.result.jsConfig,
+          //   success(res) {
+          //     console.log(res);
+          //   },
+          // });
         }
       }).catch((error) => {
         alert(error.response.data.msg);
