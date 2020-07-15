2e42201e9c5cdd1a0238d49264533088c3b16d0e
lzq <1468743505@qq.com>
2020-07-15 10:18:00 +0800

    完善测试账号自动登录

--- public/config.js
+++ public/config.js
@@ -1,3 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-window.tenantId = 1;
-window.API_URL = 'http://192.168.18.123:6601/';
+window.tenantId = 1; // 商户 ID
+window.API_URL = 'http://192.168.18.123:6601/'; // API 地址
+window.TEST_USERNAME = '16666666666'; // 测试账号
+window.TEST_PASSWORD = '123456'; // 测试账号密码
+window.TEST_ENABLED = true; // 启用测试账号自动登录
