c22178708015f91d49ba146cae6680e36f090ff9
付佳欢 <1208761178@qq.com>
2020-07-16 17:08:45 +0800

    新闻显示

--- src/views/Index/HomeB/Mask.vue
+++ src/views/Index/HomeB/Mask.vue
@@ -9,32 +9,39 @@
         <router-link to="/news-list" class="but">立即前往</router-link>
       </div>
       <div class="close">
-        <img  @click="close" src="@/assets/Index/HomeB/Mask/x.png" alt="">
+        <img @click="close" src="@/assets/Index/HomeB/Mask/x.png" alt="" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
+
 export default {
   data() {
     return {
       isMask: false,
+      newsLength: 0,
     };
   },
   methods: {
     close() {
       const date = new Date();
       const time = String(date.getFullYear()) + String(date.getMonth()) + String(date.getDate());
-      if (localStorage.getItem('newsMask') !== time && !this.isMask) {
+      if (localStorage.getItem('newsMask') !== time && !this.isMask && this.newsLength >= 1) {
         this.isMask = true;
+      } else if (this.newsLength >= 1) {
+        localStorage.setItem('newsMask', time);
+        this.isMask = false;
       } else {
         this.isMask = false;
-        localStorage.setItem('newsMask', time);
       }
     },
   },
-  created() {
+  async created() {
+    const response = await Axios.get('/api/article/list', { params: { limit: 1 } });
+    this.newsLength = response.data.data.length;
     this.close();
   },
 };
@@ -44,7 +51,7 @@ export default {
 .mask {
   height: 100%;
   width: 100%;
-  background-color: rgba($color: #000000, $alpha: .2);
+  background-color: rgba($color: #000000, $alpha: 0.2);
   position: fixed;
   z-index: 1000;
   .content {
@@ -58,20 +65,20 @@ export default {
       background-color: #fff;
       border-radius: 2.4vw;
       padding-bottom: 5.07vw;
-      background-image: url('~@/assets/Index/HomeB/Mask/tu.png');
+      background-image: url("~@/assets/Index/HomeB/Mask/tu.png");
       background-size: 67.6vw 42.53vw;
       background-repeat: no-repeat;
       background-position: bottom;
       .top {
         width: 100%;
         height: 22.4vw;
-        background-image: url('~@/assets/Index/HomeB/Mask/bj1.png');
+        background-image: url("~@/assets/Index/HomeB/Mask/bj1.png");
         background-size: cover;
         background-repeat: no-repeat;
       }
       .title {
         font-size: 4vw;
-        color: #FF450B;
+        color: #ff450b;
         font-weight: bold;
         text-align: center;
         margin-top: 2.8vw;
@@ -79,7 +86,7 @@ export default {
       .cover {
         width: 47.6vw;
         height: 24.93vw;
-        background-image: url('~@/assets/Index/HomeB/Mask/3587.png');
+        background-image: url("~@/assets/Index/HomeB/Mask/3587.png");
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center center;
@@ -87,7 +94,7 @@ export default {
       }
       .text {
         font-size: 3.2vw;
-        color: #FF450B;
+        color: #ff450b;
         text-align: center;
         margin-top: 2.53vw;
       }
@@ -99,7 +106,7 @@ export default {
         height: 10.93vw;
         color: #fff;
         font-size: 4vw;
-        background-color: #FF450B;
+        background-color: #ff450b;
         margin: 8.53vw auto 0;
         border-radius: 5.47vw;
       }
